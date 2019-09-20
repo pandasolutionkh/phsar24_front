ad8e3a47f5a047e492c6a2a169d417b4bd3f5941
MyPhsar <myphsar.asia@gmail.com>
2019-09-20 15:43:37 +0700

    text search - remove outline shadow

--- resources/views/layouts/account.blade.php
+++ resources/views/layouts/account.blade.php
@@ -22,7 +22,7 @@
     <link rel="stylesheet" href="/css/animate.css"/>
     @yield('style')
     @php
-        $_v = '1.0.5';
+        $_v = '1.0.6';
     @endphp
     <link rel="stylesheet" href="/css/nprogress.css"/>
     <link rel="stylesheet" href="/css/app.css?v={!! $_v !!}"/>
