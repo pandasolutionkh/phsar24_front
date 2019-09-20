00f87ed28b5687a2580970ad8a6d0d6d67562c5c
MyPhsar <myphsar.asia@gmail.com>
2019-09-20 11:50:40 +0700

    update version of header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
     <link rel="stylesheet" href="/css/animate.css"/>
     @yield('style')
     @php
-        $_v = '1.0.1';
+        $_v = '1.0.2';
     @endphp
     <link rel="stylesheet" href="/css/nprogress.css"/>
     <link rel="stylesheet" href="/css/app.css?v={!! $_v !!}"/>
