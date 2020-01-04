3bcf5054c859b93e2cc7db3406d2f85e17962b8b
PandaSolution <veasna9.developer@gmail.com>
2020-01-04 12:21:29 +0700

    update sub-category menu of mobile

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,7 +40,7 @@
     <link rel="stylesheet" href="/css/animate.css"/>
     @yield('style')
     @php
-        $_v = '1.2.1';
+        $_v = '1.2.2';
     @endphp
     <link rel="stylesheet" href="/css/nprogress.css"/>
     <link rel="stylesheet" href="/css/app.css?v={!! $_v !!}"/>
