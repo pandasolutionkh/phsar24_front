6e6fc13245b4262b9d85b590f5f17c1031f0d1f3
PandaSolution <veasna9.developer@gmail.com>
2020-01-12 12:34:16 +0700

    update layout of style

--- resources/views/layouts/account.blade.php
+++ resources/views/layouts/account.blade.php
@@ -21,7 +21,7 @@
     <link rel="stylesheet" href="/css/animate.css"/>
     @yield('style')
     @php
-        $_v = '1.4.3';
+        $_v = '1.4.4';
     @endphp
     <link rel="stylesheet" href="/css/nprogress.css"/>
     <link rel="stylesheet" href="/css/app.css?v={!! $_v !!}"/>
