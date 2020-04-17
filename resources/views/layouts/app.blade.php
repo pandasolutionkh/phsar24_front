735c0cec708b25d6994cb40767f5effdd22a793c
PandaSolution <veasna9.developer@gmail.com>
2020-04-17 15:50:43 +0700

    remove facebook chat, it does not support with http

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -76,7 +76,6 @@
     @yield('script')
 
     @if(config('app.env') == 'production')
-    @include('layouts.facebook')
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-148358363-1"></script>
     <script>
