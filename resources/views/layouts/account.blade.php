948b2d0dd5a09322f52255779e83334486c64262
PanaSolution <veasna9.developer@gmail.com>
2020-11-13 15:54:19 +0700

    update image detail

--- resources/views/layouts/account.blade.php
+++ resources/views/layouts/account.blade.php
@@ -21,7 +21,7 @@
     <link rel="stylesheet" href="/css/animate.css"/>
     @yield('style')
     @php
-        $_v = '1.6.0';
+        $_v = '1.6.1';
     @endphp
     <link rel="stylesheet" href="/css/nprogress.css"/>
     <link rel="stylesheet" href="/css/app.css?v={!! $_v !!}"/>
